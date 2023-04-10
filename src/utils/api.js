26375aaf2b84290e187adce9bda183eea2d9c8e9
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-04-10 18:22:22 +0200

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,11 @@
 import { request } from "./auth";
 
-const baseURL = "http://localhost:3001";
+// const baseURL = "http://localhost:3001";
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.miguelmarketer.com"
+    : "http://localhost:3001";
 
 const generateGradient = (text) => {
   return request(`${baseURL}/gradient`, {
