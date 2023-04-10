c07d23b72ebaa59deb0f6c4f5e9dac9bf195a10b
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-04-10 22:04:58 +0200

    delete

--- src/utils/errorMessages.js
+++
@@ -1,8 +0,0 @@
-const errorMessages = {
-  email: "Please enter a valid email address",
-  firstname: "Please enter your first name",
-  lastname: "Please enter your last name",
-  message: "Please enter a message (140 characters maximum)",
-};
-
-export default errorMessages;
