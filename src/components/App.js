6ecb9e03295e970a669b3de2b4083b499b9de809
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-10-21 11:48:32 +0200

    helmet

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 import Header from "./Header";
 import HeaderBurger from "./HeaderBurger";
@@ -19,32 +20,34 @@ import "../blocks/App.css";
 
 function App() {
   return (
-    <main className="app-container">
-      <Router>
-        <ScrollToTop />
-        <div className="page">
-          <div className="page__content">
-            <Header />
-            <HeaderBurger />
-            <Routes>
-              <Route path="/" element={<PageContent />} />
-              <Route path="/sobre-mi" element={<About />} />
-              <Route path="/mi-trayectoria" element={<Timeline />} />
-              <Route path="/contacta" element={<Contact />} />
-              <Route path="/blog" element={<BlogList />} />
-              <Route path="/blog/:postId" element={<BlogPost />} />
-              <Route exact path="/blog-posts" element={<NewBlogPosts />} />
-              <Route
-                exact
-                path="/blog-posts/:slug"
-                element={<BlogPostDetail />}
-              />
-            </Routes>
-            <Footer />
+    <HelmetProvider>
+      <main className="app-container">
+        <Router>
+          <ScrollToTop />
+          <div className="page">
+            <div className="page__content">
+              <Header />
+              <HeaderBurger />
+              <Routes>
+                <Route path="/" element={<PageContent />} />
+                <Route path="/sobre-mi" element={<About />} />
+                <Route path="/mi-trayectoria" element={<Timeline />} />
+                <Route path="/contacta" element={<Contact />} />
+                <Route path="/blog" element={<BlogList />} />
+                <Route path="/blog/:postId" element={<BlogPost />} />
+                <Route exact path="/blog-posts" element={<NewBlogPosts />} />
+                <Route
+                  exact
+                  path="/blog-posts/:slug"
+                  element={<BlogPostDetail />}
+                />
+              </Routes>
+              <Footer />
+            </div>
           </div>
-        </div>
-      </Router>
-    </main>
+        </Router>
+      </main>
+    </HelmetProvider>
   );
 }
 
