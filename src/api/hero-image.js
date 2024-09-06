25a49451187cdbe6ef03d5d19408c382310a2826
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2024-09-06 08:35:50 +0200

    heroimage

--- src/api/hero-image.js
+++ src/api/hero-image.js
@@ -9,29 +9,32 @@ export default async function handler(req, res) {
     console.log(`Fetching Unsplash API from URL: ${apiUrl}`);
 
     const response = await fetch(apiUrl);
-
     console.log(`Response status: ${response.status}`);
+    console.log(`Response headers: ${JSON.stringify(response.headers.raw())}`);
 
-    const data = await response.json();
+    // Verifica si la respuesta es JSON
+    const contentType = response.headers.get("content-type");
+    console.log(`Content type of response: ${contentType}`);
 
-    console.log("Fetched data from Unsplash API:", data);
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      console.log("Fetched data from Unsplash API:", data);
 
-    if (response.status !== 200) {
-      throw new Error(`Error fetching data: ${response.statusText}`);
+      const imageUrl = data.urls.full;
+      const imageAuthor = data.user.name;
+      const imageAuthorUrl = data.user.links.html;
+      const imageAuthorProfileImage = data.user.profile_image.medium;
+
+      res.status(200).json({
+        imageUrl,
+        imageAuthor,
+        imageAuthorUrl,
+        imageAuthorProfileImage,
+      });
+    } else {
+      console.error("Received non-JSON response from Unsplash API.");
+      res.status(500).json({ error: "Invalid response format from Unsplash" });
     }
-
-    const imageUrl = data.urls.full;
-    const imageAuthor = data.user.name;
-    const imageAuthorUrl = data.user.links.html;
-    const imageAuthorProfileImage = data.user.profile_image.medium;
-
-    // Devuelve la imagen y detalles del autor
-    res.status(200).json({
-      imageUrl,
-      imageAuthor,
-      imageAuthorUrl,
-      imageAuthorProfileImage,
-    });
   } catch (error) {
     console.error("Error in fetching hero image:", error);
     res.status(500).json({ error: "Error fetching hero image" });
--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -3,10 +3,10 @@ import emojis from "../emoji.json";
 import "../blocks/Hero.css";
 
 const HeroComponent = () => {
-  const baseURL =
-    process.env.NODE_ENV === "production"
-      ? "https://api.miguelmarketer.com"
-      : "http://localhost:3001";
+  // const baseURL =
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://api.miguelmarketer.com"
+  //     : "http://localhost:3001";
 
   const getRandomEmoji = () => {
     const randomIndex = Math.floor(Math.random() * emojis.length);
@@ -66,7 +66,7 @@ const HeroComponent = () => {
     };
 
     fetchHeroImage();
-  }, [baseURL]);
+  }, []);
 
   const backupImageUrl =
     "https://images.unsplash.com/photo-1672243776760-67aec979f591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
