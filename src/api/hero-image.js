e922650fd66c45ba9f3f5b25205b140382398c48
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2024-09-11 08:14:50 +0200

    nuevo hero-image

--- src/api/hero-image.js
+++ src/api/hero-image.js
@@ -1,9 +1,8 @@
 // src/api/hero-image.js
 
 export default async function handler(req, res) {
-  console.log("Serverless function hero-image invoked"); // Log inicial
-  console.log("Simple test log");
-  // res.status(200).json({ message: "Function is working!" });
+  console.log("Serverless function hero-image invoked");
+
   const accessKey = process.env.UNSPLASH_ACCESS_KEY;
   const collections = "Wallpapers";
   const apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&collections=${collections}&orientation=landscape&content_filter=high`;
@@ -13,38 +12,40 @@ export default async function handler(req, res) {
 
     const response = await fetch(apiUrl);
     console.log(`Response status: ${response.status}`);
-    console.log(`Response headers: ${JSON.stringify(response.headers.raw())}`);
+    console.log(`Response headers: ${JSON.stringify([...response.headers])}`);
 
     // Verifica si la respuesta es JSON
     const contentType = response.headers.get("content-type");
     console.log(`Content type of response: ${contentType}`);
 
     const responseBody = await response.text(); // Lee la respuesta como texto
-    console.log("Response body as text:", responseBody); // Log del cuerpo de la respuesta
-
-    // Si la respuesta es JSON, la parseamos
+    console.log("Response body as text:", responseBody);
 
     if (contentType && contentType.includes("application/json")) {
-      const data = await response.json();
-      console.log("Fetched data from Unsplash API:", data);
+      const data = JSON.parse(responseBody); // Parseamos el JSON manualmente
+      console.log("Parsed JSON from Unsplash API:", data);
 
       const imageUrl = data.urls.full;
       const imageAuthor = data.user.name;
       const imageAuthorUrl = data.user.links.html;
       const imageAuthorProfileImage = data.user.profile_image.medium;
 
-      res.status(200).json({
+      // Enviar la respuesta una vez que tenemos los datos
+      return res.status(200).json({
         imageUrl,
         imageAuthor,
         imageAuthorUrl,
         imageAuthorProfileImage,
       });
     } else {
+      // Si no es JSON, reportamos el error
       console.error("Received non-JSON response from Unsplash API.");
-      res.status(500).json({ error: "Invalid response format from Unsplash" });
+      return res
+        .status(500)
+        .json({ error: "Invalid response format from Unsplash" });
     }
   } catch (error) {
     console.error("Error in fetching hero image:", error);
-    res.status(500).json({ error: "Error fetching hero image" });
+    return res.status(500).json({ error: "Error fetching hero image" });
   }
 }
