a15c9765065bc557e525dceeca2ee331ee8514a4
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2024-09-11 08:52:16 +0200

    nuevo test

--- src/api/hero-image.js
+++ src/api/hero-image.js
@@ -2,7 +2,29 @@
 
 export default async function handler(req, res) {
   console.log("Serverless function hero-image invoked");
-  return res.status(200).json({ message: "Function is working!" });
+
+  const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+  const collections = "Wallpapers";
+  const apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&collections=${collections}&orientation=landscape&content_filter=high`;
+
+  try {
+    console.log(`Fetching Unsplash API from URL: ${apiUrl}`);
+
+    const response = await fetch(apiUrl);
+    console.log(`Response status from Unsplash: ${response.status}`);
+
+    const contentType = response.headers.get("content-type");
+    console.log(`Content type of response: ${contentType}`);
+
+    const responseBody = await response.text();
+    console.log("Response body as text:", responseBody);
+
+    // Envía la respuesta de la API directamente al cliente para depurar
+    return res.status(200).send(responseBody);
+  } catch (error) {
+    console.error("Error in fetching hero image:", error);
+    return res.status(500).json({ error: "Error fetching hero image" });
+  }
 }
 
 // export default async function handler(req, res) {
